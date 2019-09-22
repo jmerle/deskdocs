4d8eb7de749ab82f82960a4429ad4375dfb7ea28
Jasper van Merle <jaspervmerle@gmail.com>
2019-09-22 12:18:35 +0200

    Force desktop layout

--- src/webview/index.ts
+++ src/webview/index.ts
@@ -14,20 +14,49 @@ function onNavigate(modules: Module[], currentPathname: string): void {
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const interval = setInterval(() => {
-    if (app !== undefined && app.router !== undefined) {
-      clearInterval(interval);
+function forceDesktopLayout(): void {
+  const mobileChecks = ['(max-width: 480px)', '(max-width: 767px)', '(max-height: 767px) and (max-width: 1024px)'];
 
-      const modules = createModules();
+  const originalMatchMedia = window.matchMedia;
 
-      if (app.router.context !== undefined) {
-        onNavigate(modules, window.location.pathname);
-      }
-
-      app.router.on('after', (type: string, details: any) => {
-        onNavigate(modules, details.path);
-      });
+  // @ts-ignore
+  // tslint:disable-next-line:only-arrow-functions
+  window.matchMedia = function(): any {
+    if (mobileChecks.includes(arguments[0])) {
+      return {
+        matches: false,
+      };
     }
-  }, 50);
+
+    return originalMatchMedia.apply(this, arguments as any);
+  };
+}
+
+function waitUntilAppLoaded(): Promise<void> {
+  return new Promise(resolve => {
+    const checkForApp = () => {
+      if (app !== undefined && app.router !== undefined) {
+        resolve();
+      } else {
+        requestAnimationFrame(checkForApp);
+      }
+    };
+
+    requestAnimationFrame(checkForApp);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  forceDesktopLayout();
+
+  await waitUntilAppLoaded();
+  const modules = createModules();
+
+  if (app.router.context !== undefined) {
+    onNavigate(modules, window.location.pathname);
+  }
+
+  app.router.on('after', (type: string, details: any) => {
+    onNavigate(modules, details.path);
+  });
 });
